5f8d29a4a106ceebeff36e97747f0879b9e89b12
Martin Muriuki <helheim254@gmail.com>
2023-06-17 12:26:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+const totalBatteries = batteryBatches.reduce(function(sum, batches) {
+    sum+=batches;
+    return sum;
+});
 // Code your solution here
